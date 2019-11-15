0bc674c3ae26f60eb79f816850c665550f86c182
Juho Suominen <juho.suominen@grano.fi>
2019-11-15 17:59:32 +0200

    Use https instead of node-fetch

--- src/function.js
+++ src/function.js
@@ -1,5 +1,11 @@
-const fetch = require('node-fetch');
+const https = require('https');
 
-module.exports = function () {
-	return fetch('https://github.com').then(res => res.text());
+module.exports = async function () {
+	return new Promise(resolve => {
+		let data = '';
+		https.get('https://github.com', res => {
+			res.on('data', chunk => data += chunk);
+			res.on('end', () => resolve(data));
+		})
+	});
 };
